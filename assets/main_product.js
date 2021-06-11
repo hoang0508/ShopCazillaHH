a22dc0253bae906be739cacac269bef1957c2448
Huy Hoang <nguyenhuyhoang05082001@gmail.com>
2021-06-11 23:14:50 +0700

    nh

--- assets/main_product.js
+++ assets/main_product.js
@@ -3,7 +3,7 @@ function changeImage(id) {
     document.getElementById('img-main').setAttribute('src', imagePath);
 }
 
-var countDate = new Date('June 20, 2021 00:00:00').getTime();
+var countDate = new Date('June 25, 2021 00:00:00').getTime();
 
 function newYear() {
     var now = new Date().getTime();
@@ -28,3 +28,10 @@ function newYear() {
 setInterval(function() {
     newYear();
 }, 1000)
+
+// 1 năm = 365 * 24 * 60 * 60 = 31536000
+// 1 tháng = 31 * 24 * 60 * 60 = 2678400
+// 1 tuần = 7 * 24 * 60 * 60 = 604800
+// 1 ngày = 1 * 24 * 60 * 60 = 86400
+// 1 giờ = 1 * 60 * 60 = 3600
+// 1 phút = 1 * 60 = 60
